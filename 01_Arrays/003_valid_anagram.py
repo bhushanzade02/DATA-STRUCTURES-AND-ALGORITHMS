6eec7cd2854c1bb88bfc2e7c31df24beb82790fc
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2026-03-24 21:58:33 +0530

    Update 003_valid_anagram.py

--- 01_Arrays/003_valid_anagram.py
+++ 01_Arrays/003_valid_anagram.py
@@ -4,7 +4,7 @@ class Solution:
         for char in t :
             if char not in s_counts:
                 return False
-            s_counts[char] -=1
+            s_counts[char] -= 1
             if s_counts[char] == 0 :
                 del s_counts[char]
         return len(s_counts) == 0 
