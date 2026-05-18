2c88e8b2af891fc21b6226fcfffb92616a2288af
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2026-05-18 11:42:09 +0530

    Create 0002_while_loop.py

---
+++ 0002_while_loop.py
@@ -0,0 +1,11 @@
+class Solution:
+    def whileLoop(self, d : int) -> int:
+        total= d
+        sum = d 
+        # sum = d
+        i = d
+        while i < 50 :
+            total = total + 10 
+            sum = sum + total 
+            i += 1
+        return sum 
