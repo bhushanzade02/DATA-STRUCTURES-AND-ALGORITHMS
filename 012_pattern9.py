a719e336294d37a8f6cdc88b14f1175113a54782
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2026-05-22 14:35:39 +0530

    Add pattern9
    Implemented pattern9 method to print a specific star pattern based on input n.

---
+++ 012_pattern9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pattern9(self, n):
+        for i in range(1,n+1):
+        for j in range(n-i):
+            print(" ", end = '')
+        for j in range(2*i-1):
+            print("*",end = '')
+        # for j in range(n-i-1):
+        #     print(" ", end= '')
+        print()
+
+
+        for i in range(n):
+            for j in range(i):
+                print(" ", end = '')
+            for j in range(2*n-(2*i+1)):
+                print("*", end = '')
+            # for j in range(i):
+            #     print(" ", end = '')
+            print()
