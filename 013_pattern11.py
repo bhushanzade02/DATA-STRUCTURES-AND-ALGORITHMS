a6697e716e33035e002e4b1013f4c79c80256b98
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2026-05-24 23:41:10 +0530

    Implement pattern11

---
+++ 013_pattern11.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pattern11(self, n):
+
+        for i in range(n):
+
+            if i % 2 == 0:
+                start = 1
+            else:
+                start = 0
+
+            for j in range(i + 1):
+                print(start, end=' ')
+                start = 1 - start
+
+            print()
