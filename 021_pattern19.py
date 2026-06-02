0afcaee88d34ec4b34f95a5b0f011375a02c39a1
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2026-06-02 17:12:47 +0530

    Implement pattern19

---
+++ 021_pattern19.py
@@ -0,0 +1,25 @@
+class Solution:
+    def pattern19(self, n):
+        for i in range(n):
+            for j in range(n-i):
+                print("*",end = "")
+
+            for j in range(i * 2):
+                print(" ", end = '')
+
+            for j in range(n-i):
+                print('*',end = "")
+            print()
+
+        for i in range(1,n+1):
+
+            for j in range (i):
+                print("*", end = '')
+
+            for j in range(2*(n-i)):
+                print(" ", end ='')
+
+            for j in range(i):
+                print('*',end = '')
+            print()
+            
